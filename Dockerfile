60917bddbd877b6b0e118674fb1904c48ed3d6a8
b3cecchini <b3cecchini@gmail.com>
2024-11-25 19:38:31 -0500

    Gateway Project

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM mcr.microsoft.com/dotnet/aspnet:8.0-alpine AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+RUN apk update
+RUN apk upgrade
+RUN apk add curl
+RUN apk add --no-cache icu-libs
+ENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=false
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0-alpine AS build
+WORKDIR /
+COPY ["om-svc-gateway/om-svc-gateway.csproj", "./"]
+RUN dotnet restore "om-svc-gateway.csproj"
+COPY . .
+
+FROM build AS publish
+RUN dotnet publish "om-svc-gateway/om-svc-gateway.csproj" -c Release -o /app
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app .
---
+++ om-svc-gateway/Program.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Yarp.ReverseProxy.Configuration;
+using Yarp.ReverseProxy.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+// Console.WriteLine("*******************************");
+// Console.WriteLine("*******************************");
+// Console.WriteLine(" ==> Config values: ");
+// Console.WriteLine("Authority: " + builder.Configuration["Authentication:JwtBearer:Authority"]);
+// Console.WriteLine("Audience: " + builder.Configuration["Authentication:JwtBearer:Audience"]);
+// Console.WriteLine("*******************************");
+// Console.WriteLine("*******************************");
+
+builder.Services.AddAuthentication(options => 
+    {
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["Authentication:JwtBearer:Authority"];
+        options.RequireHttpsMetadata = false;
+        options.Audience = builder.Configuration["Authentication:JwtBearer:Audience"];
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            RequireSignedTokens = true,
+        };
+    });
+
+
+
+/*builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("default", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("roles", "user-web", "admin");
+    });
+});*/
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapReverseProxy(proxy =>
+{
+    proxy.UseAuthorization();
+    proxy.UseRouting();
+});
+
+app.MapHealthChecks("/health");
+
+app.UseCors("CorsPolicy");
+
+app.Run();
